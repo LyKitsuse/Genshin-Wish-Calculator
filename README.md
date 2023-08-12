97008a74437f4482a3ef5deff91758cae371a295
LykusKitsuse <gsporlanda@gmail.com>
2023-08-12 21:24:50 +0800

    Updated("JS"): Added more vars and fateConv func

--- README.md
+++ README.md
@@ -3,32 +3,56 @@ A Genshin Impact 5 Star Predictor and Primogem Calculator which outputs the odds
 
 # Things to Keep in mind! (Dev Notes)
 For this to work, we must be able to determine what the:
-- Current Pity
-- Current Primogem Count
-- Current Intertwined Fate Count
+- Current Pity = To Be Input by the User
+- Current Primogem Count = To Be Input by the User
+- Current Intertwined Fate Count = To Be Input by the User
 - Current Stardust + Starglitter count
 - Wishing date (Can also be days left, maybe?)
-- Events
+
+- Events = 420 Average per Event + 3 Events per Patch
+- Main Event = Assuming 720 Primogems per Patch
 
 Other things that can influence the outcome are:
-- Current Abyss Stars (Floors 9 - 12)
-- Welkin ownership (30 days)
-- Battlepass ownership 
-- 
+- Current Abyss Stars (Floors 9 - 12) = 150 Primogems per floor.
+- Welkin ownership (30 days per Welkin) (Will only take 1 Welkin Moon)
+- Battlepass ownership = 680 Primogems + 4 Fates (per patch) 
 
 Fixed rewards + content/events:
-- Maintenance
-- Livestreams
+- Maintenance = 600 Primogems (per patch)
+- Livestreams = 300 primogems (per patch)
+- Character Trials = 20 Primogems per Character + 4 Characters per update.
+- Daily Comission Rewards = 60 Primogems per day;
+- If 50/50 is lost, then + 10 Starglitter
 
-Miscellaneous:
+Miscellaneous (ADD LATER):
 - Web Events
 - Hoyolab
 
-# Numbers and Formula to be used
-Normal:
+# Conversions and Info
+- 160 Primogemmss = 1 Intertwined Fate
+- 5 Starglitter = 1 Intertwined Fate
+- 75 Stardust = 1 Intertwined Fate (Max 5 per month (30 Days))
 
-With Welkin:
+- 1 3-Star Weapon = 15 Stardust (Needs 5 3-Star Wishes for 1 Converted IF)
+- 1 4-Star Weapon = 2 Starglitter (Needs 3 wishes [Excess 1 Starglitter] for 1 Converted IF) (In every )
 
-With Battlepass:
+- In every 10 Wishes there is a 4-Star Character/Weapon Guaranteed (Assume all chracters return 2 Starglitter)
+- In every 75 Wishes there is a chance for a 5 Star.
+- In every 90 Wishes there is a guarnteed 5 Star.
+- In every 50/50 Loss, the next one is guaranteed.  (Concept)
+- Assuming in every 5th 50/50, it loses.  (Concept)
+- Each patch lasts up to 42 days.
+- Maintenance and Livestreams will only count every (patch or 42 days), if below 42 days then do not include.
 
-With Battlepass + Welkin:
+# Expected Outputs
+- For Full Cycles (Per 75 Pulls)
+- For Quarter Cycles (Per 75 Pulls)
+- With/Without Welkin
+- With/Without BattlePass
+- With/Without including Starglitter
+- With/Without including Stardust
+- Depending on Abyss
+
+# To add
+- Hoyolab
+- Option to include Starglitter and Stardust
--- script.js
+++ script.js
@@ -1,2 +1,83 @@
-const maintenanceRewards = 600;
-const liveRewards = 300;
+const maintenanceRewards = 600; // Maintenance Rewards
+const liveRewards = 300;        // Livestrema Rewards
+const events = 420 * 3;         // Minor Event
+const mainEvent = 740;          // Major Event
+const dailiesRewards = 60;      // Every Day Comissions
+const characterTrials = 20 * 4; // 4 Character Trials per Patch
+
+const primoConversion = 160;    // Primo to Fates
+const patchDays = 42;           // Number of Days per Update
+const softPity = 74;            // Soft Pity Count
+const hardPity = 90;            // Hard Pity Count
+
+let battlepassOwnership = false;  // If Owns the Battle Pass      REQUIRES INPUT
+let welkinOwnership = false;      // If Owns Welkin               REQUIRES INPUT
+let fiftyFifty = false;
+
+let currentPrimo = 0;     // Current Primogem Count               REQUIRES INPUT
+let currentPity = 0;      // Current Pity Count                   REQUIRES INPUT
+let ifCount = 0;          // Current Intertwined Fate Count       REQUIRES INPUT
+let stardustCount = 0;    // Current Stardust Count               REQUIRES INPUT
+let starglitterCount = 0; // Current Starglitter Count            REQUIRES INPUT
+let numDays = 0           // Predicted Wishing Date               REQUIRES INPUT
+
+function primoToFates(currentPrimo, primoConversion) {   // Converts the input PrimoCount to Fates
+  return currentPrimo / primoConversion;
+}
+
+// TODO: Make a converion for days to fate according to Maintenance, 
+// livestreams, events, character trials, and daily comissions.
+// Also add if welkin and battlepass is included.
+function fateConversion(days) {
+  let output = 0;
+  let patchCount = days / patchDays;
+
+  output += days * dailiesRewards;  // Days multiplied by the comissions rewards
+  
+  // TODO: A loop to check if there are more patch days,
+  for (let i = 0; i < patchCount; i++) {
+    output += liveRewards + maintenanceRewards;
+    output += events + mainEvent;
+    output += characterTrials;
+  }
+
+  if (welkinOwnership) {
+    if (days >= 30) {
+      output += 90 * 30;
+    }
+    else {
+      output += 90 * days;
+    }
+  }
+
+  if (battlepassOwnership) {
+    output += (160 * 4) + 680;
+  }
+
+  return primoToFates(output, primoConversion);
+}
+
+// TODO: Add Abyss Detector
+
+// TODO: Make a function that determines the predicted Pity of the user
+// Make it so that it shows how many Full Cycles (90 Wishes) 
+// and Quarter Cycles (75 Wishes) it will take. 
+// Also add the predicted Pity it will land on after the cycles for both (75 and 90)
+
+// Also Add the Starglitter and Stardusts counts when wishing, also apply the days for stardust
+
+
+let fateCount = primoToFates(currentPrimo, primoConversion) 
+  + fateConversion(numDays); // Estimated Total Fate Count
+
+/**
+ * Ask User for Primo Count 
+ * Ask User for Current Pity
+ * Ask User for Days Left
+ * 
+ * Optional
+ * Ask User for Star(Dust/Glitter) Count
+ * Ask User for Current Fate Count
+ * 
+ */
+
